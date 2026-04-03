ee6706d8743ad520e2d3648c5d00484b29faa0f8
Ayush7736 <imayush283@gmail.com>
2026-04-03 20:41:34 +0530

    Implement image upload and retrieval endpoints

---
+++ server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+
+const express = require("express");
+const multer = require("multer");
+const axios = require("axios");
+const FormData = require("form-data");
+
+const app = express();
+
+app.use(express.static("public"));
+
+const upload = multer();
+
+let lastImageUrl = "";
+
+app.post("/upload",upload.single("image"),async(req,res)=>{
+
+let form = new FormData();
+
+form.append("image",req.file.buffer.toString("base64"));
+
+let response = await axios.post(
+
+`https://api.imgbb.com/1/upload?key=${process.env.IMGBB_KEY}&expiration=300`,
+form,
+{headers:form.getHeaders()}
+);
+
+lastImageUrl = response.data.data.url;
+
+res.send("ok");
+
+});
+
+app.get("/latest",(req,res)=>{
+
+res.send(lastImageUrl);
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+
+console.log("running");
+
+});
